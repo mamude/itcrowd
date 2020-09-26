0b9a3d187edc2b07d1bd80cdb8a6fa9b14a8452f
Samir Mamude <mamude@gmail.com>
2020-09-26 19:34:11 -0300

    features: added fake data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Truncate tables
+DatabaseCleaner.clean_with(:truncation)
+
+# Add User Admin
+User.create(username: 'admin', password: 'admin')
+
+# Add Movies
+100.times do
+  date = Faker::Date.between(from: 50.years.ago, to: Date.today)
+  Movie.create(title: FFaker::Movie.title, release_year: date.year) do |movie|
+    # add actors
+    count = rand(1..30)
+    count.times do
+      movie.actors.build(last_name: FFaker::Name.last_name, first_name: FFaker::Name.first_name, aliases: Faker::FunnyName.name)
+    end
+
+    # add producers
+    count = rand(1..50)
+    count.times do
+      movie.producers.build(last_name: FFaker::Name.last_name, first_name: FFaker::Name.first_name, aliases: Faker::FunnyName.name)
+    end
+
+    # add directors
+    count = rand(1..3)
+    count.times do
+      movie.directors.build(last_name: FFaker::Name.last_name, first_name: FFaker::Name.first_name, aliases: Faker::FunnyName.name)
+    end
+  end
+end
